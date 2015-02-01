ba950c603038b8ecd9976025622af208debeca52
R_Richie <R_Richie@163.com>
2015-02-01 15:48:04 +0800

    file

--- src/com/r/base/io/TestFile.java
+++ src/com/r/base/io/TestFile.java
@@ -1,16 +1,28 @@
 package com.r.base.io;
 
 import java.io.File;
+import java.io.IOException;
 
 public class TestFile {
 
 	/**
 	 * @param args
+	 * @throws IOException
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println(File.pathSeparator);
 		System.out.println(File.separator);
+		File file1 = new File("Data.txt");
+		System.out.println(file1.getAbsolutePath());
+		File parent = new File("src/com/r/base");
+		File file2 = new File(parent, "/io/TestFile.java");
+		System.out.println(file2.getAbsolutePath());
+		System.out.println(file1.getCanonicalPath());
+		System.out.println(file2.getParent());
+		File[] roots = File.listRoots();
+		for (File root : roots) {
+			System.out.println(root.getAbsolutePath());
+		}
 	}
-
 }
